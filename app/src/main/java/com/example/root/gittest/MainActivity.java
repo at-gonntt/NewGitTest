a4f23b2964d328c511f0c51ad47f0156a07c09dc
gonntt <gonntt@asiantech.vn>
2016-07-19 17:30:28 +0700

    branchA/01

--- app/src/main/java/com/example/root/gittest/MainActivity.java
+++ app/src/main/java/com/example/root/gittest/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Suu doi lan 1 vao nhanh A
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
