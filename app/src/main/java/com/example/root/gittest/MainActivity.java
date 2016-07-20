874c30295f03b741d65cb3419e4fad998ff608f5
gonntt <gonntt@asiantech.vn>
2016-07-20 09:44:40 +0700

    master_01

--- app/src/main/java/com/example/root/gittest/MainActivity.java
+++ app/src/main/java/com/example/root/gittest/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //change in master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
